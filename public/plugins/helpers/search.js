2225a47123b9bb4e515670d5c6627f2c2a1ecf66
zeta <jersson.z032@gmail.com>
2025-08-24 20:44:37 -0500

    CLEAR: quitando carpeta/archivos que no se usan en la nueva estructura

--- public/plugins/helpers/search.js
+++
@@ -1,8 +0,0 @@
-function search(idsearch, idtable) {
-  $(`#${idsearch}`).on("keyup", function () {
-    let value = $(this).val().toLowerCase();
-    $(`#${idtable} tr`).filter(function () {
-      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
-    });
-  });
-}
